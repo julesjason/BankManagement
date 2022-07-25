5a5e69d5c76b55a4626e507e642439e58ed58a5a
jules <julesjason955@gmail.com>
2022-07-25 03:53:14 +0100

    DataContext model created and define

---
+++ BankManagement/Models/Bank/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankManagement.Models.Bank
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerFisrtName { get; set; }
+        public string CustomerLastName { get; set; }
+        public string CustomerEmail { set; get; }
+        public string CustomerPhoneNumber { set; get; }
+        public DateTime CustomerCreationDate { get; set; }
+        public DateTime CustomerUpdateDate { set; get; }
+        public DateTime CustomerDeleteDate { get; set; }
+        public bool CustomerState { get; set; }
+    }
+}
---
+++ BankManagement/Models/Bank/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace BankManagement.Models.Bank
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration _Configuration;
+        public DataContext( IConfiguration configuration)
+        {
+            _Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder option)
+        {
+            option.UseMySql(_Configuration.GetConnectionString("dataAccess"));
+        }
+        public DbSet<Account> accounts { get; set; }
+        public DbSet<Customer> customers { set; get; }
+        public DbSet<Transaction> transactions { set; get; }
+
+    }
+}
